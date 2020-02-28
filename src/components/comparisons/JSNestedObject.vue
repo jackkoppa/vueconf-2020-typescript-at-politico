bc7f9307ec500a696b20ae18536903208158fa73
Jack Koppa <hello@jackkoppa.com>
2020-02-28 05:11:58 -0500

    feature: Add deep nesting access examples

---
+++ src/components/comparisons/JSNestedObject.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'JSNestedObject',
+  props: {
+    nestedObj: { type: Object }
+  },
+  computed: {
+    deeplyNestedProp() {
+      return this.nestedObj.maybeThisProp.andHopefullyThis
+    }
+  }
+}
+</script>
---
+++ src/components/comparisons/TSNestedObject.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import Vue from 'vue'
+import { Movie } from '@/api/timesmovies'
+
+export default Vue.extend({
+  name: 'TSNestedObject',
+  props: {
+    nestedObj: { type: Object as () => Movie }
+  },
+  computed: {
+    deeplyNestedProp(): string {
+      return this.nestedObj.link?.type ?? ''
+    }
+  }
+})
+</script>
